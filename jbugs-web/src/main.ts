73ede400d17c975002d47ecb84a35ec869e625fb
Mara <cizmas.mara@yahoo.com>
2019-05-20 20:24:50 +0300

    Removed something

--- jbugs-web/src/main.ts
+++ jbugs-web/src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-// I love bugs!
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
