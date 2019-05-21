7335ae8fb6e0db669cd4e1b5284ce9abcba2f9f4
szelgabriella <szelgabriella91@gmail.com>
2019-05-21 19:45:28 +0300

    added username in UserDTO

--- jbugs-business/src/main/java/msg/user/entity/dto/UserDTO.java
+++ jbugs-business/src/main/java/msg/user/entity/dto/UserDTO.java
@@ -11,15 +11,25 @@ public class UserDTO {
     private String lastName;
     private String email;
     private String mobileNumber;
+    private String username;
 
     public UserDTO() {
     }
 
-    public UserDTO(String firstName, String lastName, String email, String mobileNumber) {
+    public UserDTO(String username, String firstName, String lastName, String email, String mobileNumber) {
+        this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.mobileNumber = mobileNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getFirstName() {
