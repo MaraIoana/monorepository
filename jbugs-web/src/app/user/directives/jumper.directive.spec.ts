0e48a57c732c3bbae53ed386428aff9f97e4d992
Mara <cizmas.mara@yahoo.com>
2019-05-30 19:08:14 +0300

    Comment added

--- jbugs-web/src/app/user/directives/jumper.directive.spec.ts
+++ jbugs-web/src/app/user/directives/jumper.directive.spec.ts
@@ -1,8 +1,7 @@
-import {JumperDirective} from './jumper.directive';
-
 /*describe('JumperDirective', () => {
   it('should create an instance', () => {
     const directive = new JumperDirective();
     expect(directive).toBeTruthy();
   });
 });*/
+//
