8b5e7724dd18f64d9dcb3e2f32640c1853cef8bc
Mara <cizmas.mara@yahoo.com>
2019-05-31 14:28:42 +0300

    Some commit for gitignore stuff

--- jbugs-web/src/app/user/user-add/user-add.component.ts
+++ jbugs-web/src/app/user/user-add/user-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {addUser} from "../../models/addUser.model";
 import {UserService} from "../services/user.service";
@@ -18,6 +18,7 @@ export class UserAddComponent implements OnInit {
     {name:'DEVELOPER', value:'4', checked:false},
     {name:'TESTER', value:'5', checked:false}
   ]
+  private validForm: boolean = false;
   get selectedOptions() {
     return this.roles
       .filter(opt => opt.checked)
@@ -28,6 +29,13 @@ export class UserAddComponent implements OnInit {
   ngOnInit() {
   }
 
+  validateForm() {
+    if (this.selectedOptions.length !== 0) {
+      this.validForm = true;
+    } else {
+      this.validForm = false;
+    }
+  }
   addUser(form: NgForm) {
     this.userNew.firstName = form.value.firstName;
     this.userNew.lastName = form.value.lastName;
