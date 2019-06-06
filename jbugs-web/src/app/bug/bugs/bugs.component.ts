80039e101cc7739107c4c0cb0f5efa130aed1c83
Mara <cizmas.mara@yahoo.com>
2019-06-06 18:25:41 +0300

    Some changes

--- jbugs-web/src/app/bug/bugs/bugs.component.ts
+++ jbugs-web/src/app/bug/bugs/bugs.component.ts
@@ -65,11 +65,13 @@ export class BugsComponent implements OnInit {
     const dialogRef = this.dialog.open(BugDialogComponent);
     dialogRef.afterClosed().subscribe(result => {
       if(result){
-        this.bugService.addBug(result).subscribe(response=>{
-          console.log(response);
-        });
-        this.gridApi.refreshCells();
+        console.log(result);
       }
+      //   this.bugService.addBug(result).subscribe(response=>{
+      //     console.log(response);
+      //   });
+      //   this.gridApi.refreshCells();
+      // }
     });
   }
 
