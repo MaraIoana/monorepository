4d09c166c9b81901b9dfa79c8c8d28cb27af54b4
Samfira Paul <paul_samfira@yahoo.com>
2019-06-06 20:53:48 +0300

    asdasdasd

--- jbugs-web/src/app/guards/user-management.guard.ts
+++ jbugs-web/src/app/guards/user-management.guard.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {PermissionService} from "../role/services/permission.service";
-import {state} from "@angular/animations";
 
 @Injectable({
   providedIn: 'root'
